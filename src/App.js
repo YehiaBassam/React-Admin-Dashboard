fda7afb059b9157d97e26a1d108dd51cfe6fd2f7
Yahia bassam <ybassam@2pegypt.local>
2023-07-18 12:11:27 +0300

    add | dark-light mode theme configration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
